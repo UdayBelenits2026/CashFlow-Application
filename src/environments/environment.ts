ade37b8f09f4cd89ac6e4f7c4028ce0f8050b2d5
Venkatreddy9908 <vkasani@belenits.in>
2026-08-27 15:10:19 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,9 @@
 export const environment = {
   production: false,
-  apiBaseUrl: '/api/v1',
-  spendingApiBaseUrl: '/api/v1',
-  incomeApiBaseUrl: '/api/v1',
+  apiBaseUrl: 'http://18.61.201.228:8080/api/v1',
+  spendingApiBaseUrl: 'http://18.61.201.228:8080/api/v1',
+  incomeApiBaseUrl: 'http://18.61.201.228:8080/api/v1',
   defaultUserId: '1',
   spendingDevUserId: '',
 };
-
-
-
 
